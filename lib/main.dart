ea800fe9fd5ff5b1e18f3a67dc6baa5224711bce
SahilBagadiya <bagadiya.sahil.d@gmail.com>
2022-01-03 20:18:02 +0530

    maulik

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'myapp',
+      title: 'sahil bagadiya',
       theme: ThemeData(
         accentColor: Colors.amber,
         primaryColor: Colors.indigo,
