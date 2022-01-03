f913c84a8a8297f26f6c11fb228324fd21d493c9
Maulik avaiya <avaiyamaulik8@gmail.com>
2022-01-03 21:12:33 +0530

    avaiya

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'sahil bagadiya',
+      title: 'maulik avaiya',
       theme: ThemeData(
         accentColor: Colors.amber,
         primaryColor: Colors.indigo,
